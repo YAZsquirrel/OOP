8805ad66504ae789e63a9c490efd96b1b764e774
your-bunnywrote <sloboda1505@gmail.com>
2023-12-11 22:25:54 +0700

    Реализованы линейная функция n-й степени и полином

---
+++ OOP/Functions/NLinearFunction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Functions;
+class NLinearFunction:IParametricFunction
+{
+	class InternalNLineFunction : IDifferentiableFunction
+	{
+		public IVector a;
+		public double Value(IVector point)
+		{
+			double sum = 0;
+			for(int i = 0; i < point.Count; i++)
+			{
+				sum += a[i] * point[i];
+			}
+			sum += a.Last();
+			return sum;
+		}
+		public IVector Gradient(IVector point)
+		{
+			IVector gradient = new Vector();
+
+			for (int i = 0; i<point.Count;i++)
+			{
+				gradient.Add(point[i]);
+			}
+			gradient.Add(0);
+			return gradient;
+		}
+	}
+	public IFunction Bind(IVector parameters) => new InternalNLineFunction() { a = parameters };
+}
---
+++ OOP/Functions/Polynomial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Functions;
+class Polynomial:IParametricFunction
+{
+    class InternalPolynomialFunction : IFunction
+    {
+        public IVector a;
+        public double Value(IVector point)
+        {
+            double sum = 0;
+            for(int i = a.Count - 1; i >= 0; i--)
+            {
+                sum += a[i] * Math.Pow(point[0], i);
+            }
+            return sum;
+        }
+    }
+    public IFunction Bind(IVector parameters) => new InternalPolynomialFunction() { a = parameters };
+}
+
